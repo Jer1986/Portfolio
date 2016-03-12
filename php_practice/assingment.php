0b7e9504169c512003cb7b37ad767e499ccd59f5
Jose Reyna <jose.reyna86@gmail.com>
2016-03-12 15:51:47 -0800

    Added a new assignment php file

---
+++ php_practice/assingment.php
@@ -0,0 +1,19 @@
+<?php
+    $num = 30;
+    $num +=10;
+   echo $num."<br>";
+   // modulus (real fun!)
+    $num2 = 32;
+    $num2 %= 12;
+    echo $num2."<br>";
+    $x = 30;
+    $x++;
+    echo $x;
+    echo "<br>";  
+    
+    if ($num > $num2){
+        echo "This is true 40 is greater than 31";
+    } else {
+        echo "This is false";
+    }
+?>
