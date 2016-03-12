72db7e80229e947285cefabb67e3b588ecbf0aa4
Jose Reyna <jose.reyna86@gmail.com>
2016-03-11 19:09:03 -0800

    added an if statement

--- php_practice/learning.php
+++ php_practice/learning.php
@@ -1,4 +1,14 @@
 <?php
-echo "This is a simple PHP statement";
+   
+    echo "This is a simple PHP statement<br>";
+   //this can be used to display lot of info regard the PHP installed locally phpinfo();
+   
+    $moo = "French Bulldog";
+    if($moo == "French Bulldog"){
+        echo "The ".$moo." "."is an expensive dog";
+    }else{
+        echo "Moo, he's just a regular dog";
+        
+    }
 
 ?>
