9b082286cf6cff280b3b20df0754b4980b02a0cd
Jer1986 <jose.reyna86@gmail.com>
2016-03-16 09:44:12 -0700

    added a foreach

--- php_practice/PHP_deep_dive.php
+++ php_practice/PHP_deep_dive.php
@@ -4,6 +4,27 @@
     </head>
 </html>
 <?php
-    
+     $scuba = array("Recreational"=>array("Open Water diver","air","above 130ft"),
+                   
+"Technical"=>array("mixed gas","below 130ft.","more planning than recreational diving"));
+        
+        
+        echo "One aspect of technical diving is diving ";    
+        echo $scuba ["Technical"][1]."<br>";
+        echo " A part of recreational diving is diving ";
+        echo $scuba["Recreational"][2]."<br><br>";
+        
+        $workout = array('Gym'=> array("Squats","Deadlifts","Seated Rows"),
+                         'Bodyweight'=>
+                         
+                                array("push-ups","Running","Pull-ups"));
+        
+        foreach($workout as $type => $exercise){
+            echo "<strong><br>".$type."</strong><br>";
+        
+        foreach($exercise as $kind){
+            echo $kind."<br>";
+        }
+    }
 
 ?>
