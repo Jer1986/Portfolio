2d56ab74ec3127728caa5cece723b1fe24542aad
Jose Reyna <jose.reyna86@gmail.com>
2016-03-19 09:43:52 -0700

    another string function

--- php_practice/newfile.php
+++ php_practice/newfile.php
@@ -3,4 +3,13 @@
 $newStr= "It's the Saturday and it looks like it may rain";
 $toReplace = str_replace("Saturday","weekend",$newStr);
     echo $toReplace;
+    echo "<br>";
+    
+    $oldString = array("replace","a","set of words");
+    $newString = array("use"," these ", "words instead");
+    $useThis= "We are going to ";
+    
+    $newOne = str_replace($oldString,$newString,$useThis);
+    echo $newOne;
+
 ?>
