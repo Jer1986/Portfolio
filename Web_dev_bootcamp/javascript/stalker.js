d3fc3483a3f1ec82d47179e38c9b34b720a10a5d
jer1986 <jose.reyna86@gmail.com>
2016-09-11 10:11:45 -0700

    added a simple JS calculator

--- Web_dev_bootcamp/javascript/stalker.js
+++ Web_dev_bootcamp/javascript/stalker.js
@@ -1,5 +1,10 @@
-var firstName = prompt("What is your first name?");
+/*var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 var age = prompt("How old are you?");
 console.log("Your full name is, " + firstName+" "+ lastName);
-console.log("Your are  " + age + " years old.");
+console.log("Your are  " + age + " years old.");*/
+
+//Jascript Age calculator
+
+var yourAge = prompt("How old are you?");
+alert("You are "+ yourAge*365 + " days old");
