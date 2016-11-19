e66d175ba00b0b78ce47a54ec35ac0c269c82378
jer1986 <jose.reyna86@gmail.com>
2016-11-19 12:56:27 -0800

    file changes

--- Web_dev_bootcamp/javascript/while_loops.js
+++ Web_dev_bootcamp/javascript/while_loops.js
@@ -5,30 +5,55 @@
  3. Print all odd numbers between 300 & 333
  4. Print all numbers that are divisible  by 5 & 3 that are between 5 & 50
  */
+/**********For Loops ***************/
+
+for(var i = -10; i < 20; i++){
+    console.log(i);
+}
+
+for(var j = 10; j < 40; j+=2){
+    console.log(j);
+}
+
+for(k=300; k <= 333; k+=1){
+    if(k % 2 !== 0){
+      console.log(k);
+
+    }
+}
+
+for(l=5;  l <= 50; l++){
+    if(l % 3 === 0 && l % 5 === 0){
+    console.log(l);
+    }
+}
+
+
+/*
 
 var count = -10;
 while(count <= 19) {
 console.log(count);
 count++;
 }
-/***************/
 
 var varTwo = 12;
 while(varTwo < 40){
 console.log(varTwo);
 varTwo+=2;
 }
-/*********/
+
 var varThree =301;
 while(varThree < 333){
 console.log(varThree);
 varThree +=2;
 }
-/**********/
+
 var varFour = 5;
 while(varFour <= 50){
-    if(varFour % 3 === 0 && varFour % 5 === 0){  /*  You can use mod to produce all of the negative numbers by using !===0*/
+    if(varFour % 3 === 0 && varFour % 5 === 0){  
       console.log(varFour);  
-    }
+}
     varFour++; 
 }
+*/
