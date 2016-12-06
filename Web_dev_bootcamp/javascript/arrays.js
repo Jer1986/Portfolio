5141dd3bb563fe704510fedd5012870577bd8316
jer1986 <jose.reyna86@gmail.com>
2016-12-06 09:51:13 -0800

    finished rough draft of to do list web app

--- Web_dev_bootcamp/javascript/arrays.js
+++ Web_dev_bootcamp/javascript/arrays.js
@@ -1,11 +1,15 @@
-// Array methods
-/*
-push/pop
-shift/unshift
-indexOf
-slice
-more can be found here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-*/
 
-var x= ["Jose", "Juan", "Sara", "Stephanie"];
-document.write(x[2] + " is just great!");
+var todos = [];
+var answers = prompt("What would you like to get done today?");
+
+while(answers !== "quit"){
+     if (answers === "list") {
+     console.log(todos);
+   } else if(answers === "new") {
+     var newItem = prompt("Please enter a new to do item");
+     todos.push(newItem);
+ }
+      answers = prompt("Would you like to add another item?");
+}
+     console.log("Here is your to do list!");
+     console.log(todos);
