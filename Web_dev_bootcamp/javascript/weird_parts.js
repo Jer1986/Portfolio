ec3919615ff7d138ec3c8ed56a9a0abad0fbff0c
jer1986 <jose.reyna86@gmail.com>
2017-01-17 06:16:29 -0800

    added my first IFFE

--- Web_dev_bootcamp/javascript/weird_parts.js
+++ Web_dev_bootcamp/javascript/weird_parts.js
@@ -1,9 +1,47 @@
+//
+//function largestOfFour(arr) {
+//  for(var i = 0; i < arr.length; i++) {
+//    for(var j = 0; j < arr[i].length; j++)
+//      return arr[i][j];
+//   }
+//}
+//
+//largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
-function largestOfFour(arr) {
-  for(var i = 0; i < arr.length; i++) {
-    for(var j = 0; j < arr[i].length; j++)
-      return arr[i][j];
-   }
-}
+//function a() {
+//  console.log(this);
+//  this.newVariable = "Hi there";
+//}
+//
+//a();
+//
+//console.log(newVariable);
+//
+//const d = {
+//  name: "The D object",
+//  log: function() {
+//    let thing = this;
+//    
+//    thing.name = "Object has a new name";
+//    console.log(this);// this is pointing at the object D
+//    
+//    let giveName = function(newName) {
+//      thing.name = newName;
+//    }
+//      giveName("This object has another new Name");
+//      console.log(thing);
+//  }
+//}
+//
+//d.log();
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+const firstName = "Jose";
+(function(name) {
+  let greeting = "Hello ";
+  console.log(greeting + " " + name); 
+}(firstName));// example of an Immediately Invoked Function Expression (IFFE)
+
+
+
+
